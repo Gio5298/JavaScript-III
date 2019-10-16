853c4b7b38ad19bcae644c2d9a8e6b4c053c61d7
Regino P <regino5298@gmail.com>
2019-10-16 13:01:06 -0700

    this.js principle 2 complete

--- assignments/this.js
+++ assignments/this.js
@@ -1,7 +1,7 @@
 /* The four principles of "this";
 * in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
+* 1. Window Binding: Basically the default principle that if given no rules, will be the active. Unless strict mode is active which will return      undefined.
 * 2. 
 * 3. 
 * 4. 
@@ -11,6 +11,8 @@
 
 // Principle 1
 
+// code example for Window Binding
+
 function example(){
     console.log(this.test)
 }
@@ -19,11 +21,22 @@ var test = 'Test this principal';
 
 example();
 
-// code example for Window Binding
+
 
 // Principle 2
 
 // code example for Implicit Binding
+
+let person = {
+    name: 'Shaun',
+    age: '23',
+    location: 'Los Angeles',
+    greet: function(){
+        console.log(`Hi, I'm ${this.name}. I'm ${this.age} years old and I live in ${this.location}`)
+    }
+}
+
+person.greet();
 
 // Principle 3
 
